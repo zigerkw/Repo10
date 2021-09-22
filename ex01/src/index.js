54d3c493b05ada9c17557afa38925e9403dd4c8c
zigerkw <zejdhadzic@hotmail.com>
2021-09-22 16:14:05 +0300

    prva vjezba

---
+++ ex01/src/index.js
@@ -0,0 +1,14 @@
+// Only change code below this line
+var myPet = {
+    "species": "Dog",
+    "name": "Riki",
+    "legs": 4,
+    "firends": ["ljudi", "zivotinje"],
+}
+function myFunction(myObj) {
+return myObj;
+}
+
+// Only change code above this line
+console.log(myFunction("legs")); //Change this line
+module.exports = { myPet, myFunction }
